e1505ca117f7fc5976eef10aee6b95bb0d785044
timlhote <timlhote@gmail.com>
2019-11-19 14:30:45 +0100

    All is ok

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,7 @@
 package com.wildcodeschool.wildandwizard.repository;
 
 import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.entity.Wizard;
 
 import java.sql.*;
 
@@ -10,9 +11,27 @@ public class SchoolRepository {
     private final static String DB_USER = "h4rryp0tt3r";
     private final static String DB_PASSWORD = "Horcrux4life!";
 
-    public School update(Long id, String name, Long capacity, String country) {
+    public School save(Long id, String name, Long capacity, String country) {
 
-        // TODO : update a school from the database
+        try {
+            Connection connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            PreparedStatement statement = connection.prepareStatement(
+                    "UPDATE school SET name=?, capacity=?, country=? WHERE id=?"
+            );
+            statement.setString(1, name);
+            statement.setLong(2, capacity);
+            statement.setString(3, country);
+            statement.setLong(4, id);
+
+            if (statement.executeUpdate() != 1) {
+                throw new SQLException("failed to update data");
+            }
+            return new School(id, name, capacity, country);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
